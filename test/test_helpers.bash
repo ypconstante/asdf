896deaa3f75a2f26cc6523ff1575efd971a52479
Yuri Pereira Constante <ypconstante@gmail.com>
2022-08-13 02:19:57 -0300

    test: set initial branch name
    Tests where failing when git config `init.defaultBranch` was set with a
    value different from `master`.

--- test/test_helpers.bash
+++ test/test_helpers.bash
@@ -51,7 +51,7 @@ install_mock_plugin_repo() {
   local plugin_name=$1
   local location="${BASE_DIR}/repo-${plugin_name}"
   cp -r "$BATS_TEST_DIRNAME/fixtures/dummy_plugin" "${location}"
-  git -C "${location}" init -q
+  git -C "${location}" init -q -b master
   git -C "${location}" config user.name "Test"
   git -C "${location}" config user.email "test@example.com"
   git -C "${location}" add -A
